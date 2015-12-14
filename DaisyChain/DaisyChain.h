8f7b85b5efd08fd489368ce34857ca98eb5f31f4
Ali Karagoz <mail@alikaragoz.net>
2015-12-15 00:45:22 +0100

    Initial Commit

---
+++ DaisyChain/DaisyChain.h
@@ -0,0 +1,19 @@
+//
+//  DaisyChain.h
+//  DaisyChain
+//
+//  Created by Ali Karagoz on 15/12/15.
+//  Copyright © 2015 Ali Karagoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DaisyChain.
+FOUNDATION_EXPORT double DaisyChainVersionNumber;
+
+//! Project version string for DaisyChain.
+FOUNDATION_EXPORT const unsigned char DaisyChainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DaisyChain/PublicHeader.h>
+
+
---
+++ DaisyChainTests/DaisyChainTests.swift
@@ -0,0 +1,36 @@
+//
+//  DaisyChainTests.swift
+//  DaisyChainTests
+//
+//  Created by Ali Karagoz on 15/12/15.
+//  Copyright © 2015 Ali Karagoz. All rights reserved.
+//
+
+import XCTest
+@testable import DaisyChain
+
+class DaisyChainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
