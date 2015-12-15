c6784d15112ab0266a9a31dab4067a44ca5201a1
Ali Karagoz <mail@alikaragoz.net>
2015-12-15 09:09:49 +0100

    Added tests on serial execution

--- DaisyChainTests/DaisyChainTests.swift
+++ DaisyChainTests/DaisyChainTests.swift
@@ -11,26 +11,61 @@ import XCTest
 
 class DaisyChainTests: XCTestCase {
     
+    var daisyChain: DaisyChain!
+    
     override func setUp() {
+        daisyChain = DaisyChain()
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSerialAnimation() {
+        
+        let expectation = expectationWithDescription("Animation Expectations")
+        let view: UIView = UIView()
+        var positions: [CGFloat] = []
+        
+        daisyChain.animateWithDuration(0.1, animations: {
+            view.frame.origin.x = 10
+            }, completion: { _ in
+                positions.append(view.frame.origin.x)
+        })
+        
+        daisyChain.animateWithDuration(0.1, animations: {
+            view.frame.origin.x = 20
+            }, completion: { _ in
+                positions.append(view.frame.origin.x)
+        })
+        
+        daisyChain.animateWithDuration(0.1, animations: {
+            view.frame.origin.x = 30
+            }, completion: { _ in
+                positions.append(view.frame.origin.x)
+                
+                XCTAssert(positions[0] == 10 && positions[1] == 20 && positions[2] == 30, "Animations did not perform serially")
+                expectation.fulfill()
+        })
+        
+        waitForExpectationsWithTimeout(1.0, handler: nil)
+        
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func testSerialCompletion() {
+        
+        let expectation = expectationWithDescription("Swift Expectations")
+        var counter: Int = 0
+        
+        daisyChain.animateWithDuration(2.0, animations: {}, completion: { _ in counter++ })
+        daisyChain.animateWithDuration(1.0, animations: {}, completion: { _ in counter++ })
+        daisyChain.animateWithDuration(0.5, animations: {}, completion: { _ in counter++ })
+        daisyChain.animateWithDuration(0.1, animations: {}, completion: { _ in
+            XCTAssert(counter == 3, "Animations did not call completion serially")
+            expectation.fulfill()
+        })
+        
+        waitForExpectationsWithTimeout(5.0, handler: nil)
     }
-    
 }
